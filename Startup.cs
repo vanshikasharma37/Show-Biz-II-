7c714402f0f9e3eec3a8a79833efcae0e35dd594
Vanshika Sharma <110706627+vanshikasharma37@users.noreply.github.com>
2025-02-04 15:09:50 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VS2247A6.Startup))]
+namespace VS2247A6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
