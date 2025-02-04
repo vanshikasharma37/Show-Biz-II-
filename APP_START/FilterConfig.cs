10e311fbebb16d1194f800eb273b8e0c3032e9d2
Vanshika Sharma <110706627+vanshikasharma37@users.noreply.github.com>
2025-02-04 15:17:25 -0500

    Add files via upload

---
+++ APP_START/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VS2247A6
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
